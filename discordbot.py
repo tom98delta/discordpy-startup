a5bfc5b3e5898bc491d2090b596f3328d5303e39
tom98delta <81839669+tom98delta@users.noreply.github.com>
2021-04-03 17:39:08 +0900

    rev14

--- discordbot.py
+++ discordbot.py
@@ -42,4 +42,24 @@ async def leave(ctx):
     await voice_client.disconnect()
     await ctx.send("ボイスチャンネルから切断しました。")
     
+@bot.command()
+async def play(ctx):
+    """指定された音声ファイルを流します。"""
+    voice_client = ctx.message.guild.voice_client
+
+    if not voice_client:
+        await ctx.send("Botはこのサーバーのボイスチャンネルに参加していません。")
+        return
+
+    if not ctx.message.attachments:
+        await ctx.send("ファイルが添付されていません。")
+        return
+
+    await ctx.message.attachments[0].save("tmp.mp3")
+
+    ffmpeg_audio_source = discord.FFmpegPCMAudio("tmp.mp3")
+    voice_client.play(ffmpeg_audio_source)
+
+    await ctx.send("再生しました。")
+    
 bot.run(token)
